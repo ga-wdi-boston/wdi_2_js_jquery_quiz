e2c8ca5790409599e0ac1fc22baf0f0211e5787c
aaronw3373 <aaronw3373@gmail.com>
2015-05-05 17:40:51 -0400

    fixed most things and has nice transitions now too

--- exercise/page.js
+++ exercise/page.js
@@ -6,18 +6,38 @@ function convertCtoF (tempC) {
     return tempC * 9/5 + 32;
 }
 
+function setBackground(tempInC){
+  if (tempInC < 0){
+    $('body').css('background-color', 'purple');
+  }else if (tempInC < 10){
+    $('body').css('background-color', 'blue');
+  }else if (tempInC < 20){
+    $('body').css('background-color', 'yellow');
+  }else if (tempInC < 30){
+    $('body').css('background-color', 'orange');
+  }else if (tempInC > 30){
+    $('body').css('background-color', 'red');
+  }
+}
+
 
 $(document).ready(function(){
   var far;
   var cel;
-  $('convert-to-c').click(function() {
-    far = $('input[name="temp-f"]').val();
+  $('#temp-f').keyup(function(e) {
+    if (e.which == 13){
+    far = $('#temp-f').val();
     cel = convertFtoC(far);
-    $('input[name="temp-c"]').val(cel);
+    $('#temp-c').val(cel);
+    setBackground(cel);
+  }
   });
-  $('convert-to-f').click(function() {
-    cel = $('input[name="temp-c"]').val();
-    far = convertCtoF(far);
-    $('input[name="temp-f"]').val(far);
+  $('#temp-f').keyup(function() {
+    if (e.which == 13){
+    cel = $('#temp-c').val();
+    far = convertCtoF(cel);
+    $('#temp-f').val(far);
+    setBackground(cel);
+  }
   });
 });
