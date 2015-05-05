41565f8f854d02180555b3fdf99a809a5dfa4c37
Peter-White <pwhite007@icloud.com>
2015-05-05 17:15:56 -0400

    -_-

--- exercise/page.js
+++ exercise/page.js
@@ -1,7 +1,13 @@
+$(document).ready(function(){
+
+$('#convert-to-c').click(function() {
+
 function convertFtoC (tempF) {
     return (tempF - 32) * 5/9;
 }
 
+});
 function convertCtoF (tempC) {
     return tempC * 9/5 + 32;
 }
+};
