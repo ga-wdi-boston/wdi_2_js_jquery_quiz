e511808983edbf6771a5cb5c91ca4a055ba16a2a
azitowolf <azito.wolf3@gmail.com>
2015-05-05 17:16:53 -0400

    threw error on dollar sign

--- exercise/page.js
+++ exercise/page.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+
+
+
+
 function convertFtoC (tempF) {
     return (tempF - 32) * 5/9;
 }
@@ -5,3 +10,22 @@ function convertFtoC (tempF) {
 function convertCtoF (tempC) {
     return tempC * 9/5 + 32;
 }
+
+
+$("#convert-to-c").click(function(){
+
+
+  var cInput = $('#temp-c').val();
+  var converted = convertCtoF(cInput);
+  $(".placeholder").html(converted.toString());
+});
+
+
+$("#convert-to-f").click(function(){
+  var fInput = $('#temp-f').val();
+  var converted = convertFtoC(fInput);
+  $(".placeholder").html(converted.toString());
+});
+
+
+});
