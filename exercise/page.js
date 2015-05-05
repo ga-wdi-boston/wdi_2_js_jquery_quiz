ed69816b9d9ee5a6a5adcfd04ac346fad6fe57f8
Charlton Wilbur <cwilbur@chromatico.net>
2015-05-05 16:41:40 -0400

    solns

--- exercise/page.js
+++ exercise/page.js
@@ -5,3 +5,42 @@ function convertFtoC (tempF) {
 function convertCtoF (tempC) {
     return tempC * 9/5 + 32;
 }
+
+$(document),ready(function(){
+  $('#convert-to-c').click(function(){
+    var f = $('input[name="temp-f"]').val();
+    var c = convertFtoC(f);
+    $('input[name="temp-c"]').val(c);
+
+    if (c < 0) {
+      $('body').css('background-color', 'purple');
+    } else if (c < 10) {
+        $('body').css('background-color', 'blue');
+    } else if (c < 20) {
+        $('body').css('background-color', 'yellow');
+    } else if (c < 30) {
+        $('body').css('background-color', 'orange');
+    } else {
+        $('body').css('background-color', 'red');
+    }
+  });
+
+  $('#convert-to-f').click(function(){
+    var c = $('input[name="temp-c"]').val();
+    var f = convertCtoF(c);
+    $('input[name="temp-f"]').val(f);
+
+    if (c < 0) {
+      $('body').css('background-color', 'purple');
+    } else if (c < 10) {
+        $('body').css('background-color', 'blue');
+    } else if (c < 20) {
+        $('body').css('background-color', 'yellow');
+    } else if (c < 30) {
+        $('body').css('background-color', 'orange');
+    } else {
+        $('body').css('background-color', 'red');
+    }
+  });
+
+});
