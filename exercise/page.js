eba77e5f7e8ca42f632fa339e0ce56bb594b7dd1
courtneysadusky <courtneysadusky@gmail.com>
2015-05-05 17:16:01 -0400

    changes to quiz

--- exercise/page.js
+++ exercise/page.js
@@ -2,6 +2,14 @@ function convertFtoC (tempF) {
     return (tempF - 32) * 5/9;
 }
 
+$('temp-f').val()
+var tempFinC = function(tempF){
+
+}
+
+$('#convert-to-c').click(function(){
+
+})
 function convertCtoF (tempC) {
     return tempC * 9/5 + 32;
 }
