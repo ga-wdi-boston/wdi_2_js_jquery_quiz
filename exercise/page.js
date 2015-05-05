faac45646c36dfcf50a45277e0766801a82069bf
aaronw3373 <aaronw3373@gmail.com>
2015-05-05 17:15:49 -0400

    I couldn't debug my program

--- exercise/page.js
+++ exercise/page.js
@@ -5,3 +5,19 @@ function convertFtoC (tempF) {
 function convertCtoF (tempC) {
     return tempC * 9/5 + 32;
 }
+
+
+$(document).ready(function(){
+  var far;
+  var cel;
+  $('convert-to-c').click(function() {
+    far = $('input[name="temp-f"]').val();
+    cel = convertFtoC(far);
+    $('input[name="temp-c"]').val(cel);
+  });
+  $('convert-to-f').click(function() {
+    cel = $('input[name="temp-c"]').val();
+    far = convertCtoF(far);
+    $('input[name="temp-f"]').val(far);
+  });
+});
