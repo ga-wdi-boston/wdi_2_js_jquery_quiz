6d55463661557e9bfdf4364da845a094a7e473ac
alinaw89 <ali.naw89@gmail.com>
2015-05-05 17:15:16 -0400

    not finished

--- exercise/page.js
+++ exercise/page.js
@@ -1,7 +1,27 @@
-function convertFtoC (tempF) {
+var convertFtoC = function(tempF) {
     return (tempF - 32) * 5/9;
 }
 
-function convertCtoF (tempC) {
+var convertCtoF = function (tempC) {
     return tempC * 9/5 + 32;
+}
+
+
+
+$(document).ready(function() { //not done
+  $('#convert-to-c').click(function() {
+    var userSelectF = $('input[name="temp-f"]').val();
+    convertFtoC
+
+
+ }
+
+}
+
+$(document).ready(function() { //not done
+  $('#convert-to-f').click(function() {
+    var userSelectC = $('input[name="temp-c"]').val();
+    convertCtoF
+ }
+
 }
