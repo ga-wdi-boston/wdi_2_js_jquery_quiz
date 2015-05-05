9481dedc7f670d4816ad6be61e431a7abaca7cdd
maxkrieg <maxwell.krieg@gmail.com>
2015-05-05 17:14:23 -0400

    jquery quiz completed

--- exercise/page.js
+++ exercise/page.js
@@ -1,7 +1,35 @@
-function convertFtoC (tempF) {
+var convertFtoC = function (tempF) {
     return (tempF - 32) * 5/9;
-}
+};
 
-function convertCtoF (tempC) {
+var convertCtoF = function (tempC) {
     return tempC * 9/5 + 32;
-}
+};
+
+
+
+$(document).ready(function(){
+
+  // Convert C to F, fill in F
+  $('#convert-to-f').click(function(){
+    var celsiusInput = $('#cBox').val();
+    $('#fBox').val(Math.floor(convertCtoF(celsiusInput)));
+  });
+
+  // Convert F to C, fill in C
+  $('#convert-to-c').click(function(){
+    var fahrInput = $('#fBox').val();
+    $('#cBox').val(Math.floor(convertFtoC(fahrInput)));
+  });
+
+
+
+
+
+
+
+
+
+
+
+});
