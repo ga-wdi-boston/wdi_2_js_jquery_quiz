9411baabf4f118b32e8f183a9239bf3a9333b718
amygoldberg <amykgoldberg@gmail.com>
2015-05-05 17:15:31 -0400

    commiting quiz for temp converter

--- exercise/page.js
+++ exercise/page.js
@@ -1,7 +1,27 @@
-function convertFtoC (tempF) {
+
+ $('#convert-to-c').click(function() {
+    convertCtoF();
+    // e.preventDefault();
+ })
+
+ function convertCtoF (tempC) {
+    return tempC * 9/5 + 32;
+};
+
+
+
+
+/* function convertCtoF (tempC) {
+    return tempC * 9/5 + 32;
+};
+}) */
+
+/* function convertFtoC (tempF) {
     return (tempF - 32) * 5/9;
 }
 
 function convertCtoF (tempC) {
     return tempC * 9/5 + 32;
-}
+} */
+
+
