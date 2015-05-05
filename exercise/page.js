a752277b81ee22752b7c9969371dc05d3f96f091
alik864 <alikarukas@gmail.com>
2015-05-05 17:16:14 -0400

    quiz

--- exercise/page.js
+++ exercise/page.js
@@ -5,3 +5,16 @@ function convertFtoC (tempF) {
 function convertCtoF (tempC) {
     return tempC * 9/5 + 32;
 }
+
+
+('convertCtoF').click(function)){
+$('#convertCtoF').val(32);
+$('#convertFtoC').vall(5/9);
+{
+
+    $('word-search-button').click(function()){
+var wordCount= countAllWord (clickedOnParagraph.innerText);
+var wordtoFind=$('#search-word').val();
+var thisWordFrequency = wordCount || 'none';
+
+
