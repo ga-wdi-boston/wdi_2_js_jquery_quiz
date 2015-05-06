e0422bfbf8c4399f955b563ffd4048a2aab59afa
azitowolf <azito.wolf3@gmail.com>
2015-05-05 23:06:11 -0400

    finished fahrenheit converter. hold applause

--- exercise/page.js
+++ exercise/page.js
@@ -1,8 +1,3 @@
-$(document).ready(function(){
-
-
-
-
 function convertFtoC (tempF) {
     return (tempF - 32) * 5/9;
 }
@@ -11,21 +6,42 @@ function convertCtoF (tempC) {
     return tempC * 9/5 + 32;
 }
 
+function setBackground(){
+  var tempInC = $('input[name="temp-c"]').val();
+  if(tempInC < 0){
+    $('body').css("background-color", "purple");
+  } else if(tempInC < 10){
+    $('body').css("background-color", "blue");
+  } else if(tempInC < 20){
+    $('body').css("background-color", "yellow");
+  } else if(tempInC < 30){
+    $('body').css("background-color", "orange");
+  } else if(tempInC >= 30){
+    $('body').css("background-color", "red");
+  }
+}
+
+$(document).ready(function(){
 
 $("#convert-to-c").click(function(){
-
-
-  var cInput = $('#temp-c').val();
-  var converted = convertCtoF(cInput);
-  $(".placeholder").html(converted.toString());
+  console.log("firing");
+  var fInput = $('input[name="temp-f"]').val();
+  var converted = convertFtoC(fInput);
+  $('input[name="temp-c"]').val(converted);
+  setBackground();
 });
 
 
 $("#convert-to-f").click(function(){
-  var fInput = $('#temp-f').val();
-  var converted = convertFtoC(fInput);
-  $(".placeholder").html(converted.toString());
+  console.log("firing");
+  var cInput = $('input[name="temp-c"]').val();
+  var converted = convertCtoF(cInput);
+  $('input[name="temp-f"]').val(converted);
+  setBackground();
 });
 
 
 });
+
+
+
